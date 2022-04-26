a8ae440fab4e5d6491805b384b7aa871e781b28d
jigyeong <wlrud0126@gmail.com>
2022-04-26 20:28:08 +0900

    initial webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main : './src/js/index.js',
+    },
+    output: {
+        path : path.resolve('./public'),
+        filename : '[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+    //   open: true,
+      port: 9000,
+    },
+  };
