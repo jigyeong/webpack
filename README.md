99f361d09252e90aacd667f1aa535d68381ce090
jigyeong <wlrud0126@gmail.com>
2022-04-27 17:46:33 +0900

    채팅방 생성 및 입장메시지 전송

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 npm install
 
-npm run build
-
 npm run watch
--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,15 @@ window.addEventListener('DOMContentLoaded',()=>{
     /*
      추후 localStorage 에서 data 뽑아옴
      */
-    const btnJoinRoom = document.getElementById("btnJoinRoom");
-    btnJoinRoom.addEventListener('click', joinRoom);
-
+    
 });
+const btnJoinRoom = document.getElementById("btnJoinRoom");
+btnJoinRoom.addEventListener('click', joinRoom);
 
 const users = [];
 const chatRooms = [];
 
-const joinRoom = function(){
+function joinRoom(){
     // 채팅 참가 버튼을 클릭하면 닉네임 입력 레이어가 나타납니다.
     let user = prompt('닉네임 설정');
     users.push(user);
@@ -25,33 +25,64 @@ const joinRoom = function(){
 }
 
 const openNewRoom = function(user){
-    // 채팅 리스트 UI
-    addChatList(user);
-
     // makeRoomObject
     let chatRoom = new ChatRoomObj(user);
     chatRooms.push(chatRoom);
 
-    // 입장 메시지 전송
-    const message = {
-        name : user,
+    // 새 User의 채팅방을 그린다
+    drawingNewRoom(user);
+
+    // 새 User의 입장 메시지를 모두에게 전송
+    const messageObj = {
+        user : user,
         message : 'enter',
         time : new Date().getTime()
     }
-    sendMessage(message);
+    sendMessage(messageObj);
 }
 
-const addChatList = function(name){
-    const chatList = document.getElementById('chatList');
-    let node = document.createElement('div');
-    node.innerHTML=`${name} 의 채팅방`
-    chatList.appendChild(node);
-}
-
-const sendMessage = function(message){
+const sendMessage = function(messageObj){
+    // 현재 존재하는 채팅방 foreach
     chatRooms.forEach(function(chatRoom) {
-        chatRoom.messages.push(message);
+        // 각 채팅방마다 push new message
+        chatRoom.messages.push(messageObj);
+        // 각 채팅방마다 drawing new message
+        drawingMessage(chatRoom.owner, messageObj);
     })
+}
+
+const drawingNewRoom = function(user){
+    const rootNode = document.getElementById('app');
+    const roomNode = `
+    <div class="chatRoom" id="chatRoom_${user}">
+            <div id="header" style="display: flex; text-align: center;">
+                <p>${user}</p>
+                <button style="right: 10px; position:absolute; height: 40px;">참가자</button>
+            </div>
+            <div id="messages_${user}">
+                <div style="border-radius: 0px 20px 20px 50px; background-color:lightgray; left:0; width: 50%; margin-bottom: 10px;">
+                    dd
+                </div>
+                <div style="border-radius: 20px 0px 50px 20px; background-color:pink; right: 0px; width: 50%; margin-bottom: 10px;">
+                    나의말
+                </div>
+            </div>
+            <div id="bottom" style="bottom: 10px; position: absolute; width:100%; text-align:center;">
+                <input type="text" placeholder="내용을 입력해주세요" style="width:60%"/>
+                <button>전송</button>
+            </div>
+        </div>
+    
+    `
+    rootNode.insertAdjacentHTML('beforeend',roomNode);
+}
+
+const drawingMessage = function(chatRoomOwner, messageObj){
+    let chatRoomMessagesId = `messages_${chatRoomOwner}`;
+    let chatRoomNode = document.getElementById(chatRoomMessagesId);
+    let messageDiv;
+    if(messageObj.message=='enter') messageDiv=`<div>${messageObj.user} 님이 입장하셨습니다.</div>`
+    chatRoomNode.insertAdjacentHTML('beforeend',messageDiv);
 }
 
 function ChatRoomObj(user){
