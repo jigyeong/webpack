d0948ac9949744658af93691017bd0b64a5c6053
jigyeong <wlrud0126@gmail.com>
2022-04-28 18:03:32 +0900

    참가자 명단 버튼 closure 함수 개발

--- src/js/index.js
+++ src/js/index.js
@@ -15,24 +15,18 @@ const users = [];
 const chatRooms = [];
 
 function joinRoom(){
-    // 채팅 참가 버튼을 클릭하면 닉네임 입력 레이어가 나타납니다.
     let user = prompt('닉네임 설정');
     users.push(user);
-
-    // 닉네임 입력 후 방이 생성되고 입장합니다.
     openNewRoom(user);
-
 }
 
 const openNewRoom = function(user){
-    // makeRoomObject
+
     let chatRoom = new ChatRoomObj(user);
     chatRooms.push(chatRoom);
 
-    // 새 User의 채팅방을 그린다
-    drawingNewRoom(user);
+    drawNewRoom(user);
 
-    // 새 User의 입장 메시지를 모두에게 전송
     const messageObj = {
         user : user,
         message : 'enter',
@@ -48,33 +42,53 @@ const sendMessage = function(messageObj){
         chatRoom.messages.push(messageObj);
         // 각 채팅방마다 drawing new message
         drawMessage(chatRoom.owner, messageObj);
-    })
+    });
 }
 
-const drawingNewRoom = function(user){
+const drawNewRoom = function(user){
     const rootNode = document.getElementById('app');
     const roomNode = `
     <div class="chatRoom" id="chatRoom_${user}">
-            <div id="header" style="display: flex; text-align: center;">
+            <div id="header_${user}" style="display: flex; text-align: center;">
                 <p>${user}</p>
-                <button style="right: 10px; position:absolute; height: 40px;">참가자</button>
+                <div class="divUsers" id="divUsers_${user}"></div>
             </div>
-            <div id="messages_${user}">
+            <div id="messages_${user}" class="messageBox">
             </div>
-            <div id="bottom_${user}" style="bottom: 10px; position: absolute; width:100%; text-align:center;">
+            <div id="bottom_${user}" class="bottomBox">
                 <input id="inputbox" type="text" placeholder="내용을 입력해주세요" style="width:60%"/>
             </div>
         </div>
-    `
-
-    // send button + onclickListner  별도 추가
+        `
     rootNode.insertAdjacentHTML('beforeend',roomNode);
+   
+    // Users Button
     let btnNode = document.createElement('button');
-    let btnTextNode = document.createTextNode('전송');
+    let btnTextNode = document.createTextNode('참가자');
+    btnNode.className = 'btnUsers';
+    // btnNode.addEventListener('click', clickUsersBtn);
+    btnNode.appendChild(btnTextNode);
+    const headerNode = document.getElementById('header_'+user);
+    headerNode.appendChild(btnNode);
+    
+    let divUsersNode = document.getElementById('divUsers_'+user);
+    let clickUsersBtn = (function(){
+        let isOpen = false;
+        return function(){
+            alert();
+            isOpen = !isOpen;
+            divUsersNode.style.display = isOpen ? 'block' : 'none';
+        }
+    })();
+    btnNode.onclick = clickUsersBtn;
+
+    // Send Button
+    btnNode = document.createElement('button');
+    btnTextNode = document.createTextNode('전송');
     btnNode.appendChild(btnTextNode);
     btnNode.addEventListener('click', clickSendBtn);
 
-    let bottomNode = document.getElementById('bottom_'+user);
+    const bottomNode = document.getElementById('bottom_'+user);
     bottomNode.appendChild(btnNode);
     bottomNode.children.inputbox.addEventListener('keyup', function(event) {
         event.preventDefault();
@@ -113,6 +127,7 @@ const drawMessage = function(chatRoomOwner, messageObj){
         messageDiv=`<div class="me">${messageObj.message}</div>`;
     }
     chatRoomNode.insertAdjacentHTML('beforeend',messageDiv);
+    chatRoomNode.scrollTop = chatRoomNode.scrollHeight;
 }
 
 function ChatRoomObj(user){
